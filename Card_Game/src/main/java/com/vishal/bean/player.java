40e4bc274d273526cdd71ef66e5fe7f5bd934681
vishal singh <vishalbit99@gmail.com>
2023-04-16 09:53:42 +0530

    Added properties and behaviours to player class

--- Card_Game/src/main/java/com/vishal/bean/player.java
+++ Card_Game/src/main/java/com/vishal/bean/player.java
@@ -1,5 +1,47 @@
 package com.vishal.bean;
 
-public class player {
+import java.util.*;
 
+public class player {
+	
+	//Id of the Player
+    private int playerId;
+    
+    // cards in the hand of the player.
+    private ArrayList<card> cardsInHand = new ArrayList<card>();
+
+    public player(int playerId)
+    {
+        this.playerId = playerId;
+    }
+    
+    /*
+       Adds card to players Hand
+     */
+    public void addCard(card card) {
+
+        cardsInHand.add(card);
+    }
+
+    /*
+       Removes Card from players hand
+     */
+    public void removeCard(card card) {
+        cardsInHand.remove(card);
+        return;
+    }
+    
+    // Get id of the player
+    public int giveId() {
+        return playerId;
+    }
+    
+    // Get the cards in player's hand
+    public ArrayList<card> giveCards() {
+        return cardsInHand;
+    }
+    
+    public String toString() {
+        return playerId + " " + cardsInHand.toString();
+    }
 }
